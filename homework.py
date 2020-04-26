d0cd615fa51c15aa12619ae9815913ca5eb633a0
Nik <miniakink@gmail.com>
2020-04-26 15:02:07 +0300

    Проверка отмены проверки

--- homework.py
+++ homework.py
@@ -23,30 +23,7 @@ def parse_homework_status(homework):
 
 def get_homework_statuses(current_timestamp):
     ...
-    homework_statuses = ...
-    return homework_statuses.json()
 
-
-def send_message(message):
-    ...
-    return bot.send_message(...)
-
-
-def main():
-    current_timestamp = int(time.time())  # начальное значение timestamp
-
-    while True:
-        try:
-            new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
-
-        except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
-            continue
 
 
 if __name__ == '__main__':
