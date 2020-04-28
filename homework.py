2b2ee54e52f7b2a9f14ed827e190af9e85466c87
Nik <miniakink@gmail.com>
2020-04-28 18:35:54 +0300

    Тесты пройдены 1.0

---
+++ homework.py
@@ -0,0 +1,61 @@
+import os
+import requests
+import telegram
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+proxy_url = 'socks5://72.49.49.11:31034'
+
+
+def parse_homework_status(homework):
+    homework_name = homework['homework_name']
+    status = homework['status']
+    if status != 'approved':
+        verdict = 'К сожалению в работе нашлись ошибки.'
+    else:
+        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+
+
+def get_homework_statuses(current_timestamp):
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    data = {
+        'from_date': current_timestamp,
+        'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
+    }
+    homework_statuses = requests.get(url=url, headers=headers, params=data)
+    return homework_statuses.json()
+
+
+def send_message(message):
+    proxy = telegram.utils.request.Request(proxy_url=proxy_url)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
+
+
+def main():
+    current_timestamp = int(time.time())  # начальное значение timestamp
+
+    while True:
+        try:
+            new_homework = get_homework_statuses(current_timestamp)
+            if new_homework.get('homeworks'):
+                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get('current_date')  # обновить timestamp
+            time.sleep(600)  # опрашивать раз в пять минут
+
+        except Exception as e:
+            print(f'Бот упал с ошибкой: {e}')
+            time.sleep(5)
+            continue
+
+
+if __name__ == '__main__':
+    main()
